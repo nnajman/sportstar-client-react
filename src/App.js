745fc7d6933731a120fefb05726b1484de3906c0
Nir Naiman <206769390@STUD.colman.ac.il>
2021-02-18 16:42:54 +0200

    nir - login page - 18/2/21

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/navbar/navbar';
+import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Homepage from './components/home-page/home-page.js';
 import Products from './components/products/products.js';
 import Categories from './components/categories/categories.js';
 import EditProduct from './components/products/edit.js';
 import DeleteProduct from './components/products/delete.js';
+import LoginPage from './components/login-page/login-page';
 
 function App() {
   
-  // Global variables
-  window.$lastGender = "men";
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <LoginPage setToken={setToken} />
+  }
 
   return (
     <BrowserRouter>
       <div >
         <Navbar />
-        {/* <Homepage /> */}
         <Switch>
+              {/* <Route path="/" exact component={() => <LoginPage />} /> */}
               <Route path="/" exact component={() => <Homepage />} />
-              {/* <Route path="/Home" exact component={() => <Homepage />} /> */}
               <Route path="/Products" component={() => <Products />} />
               <Route path="/EditProducts" component={() => <EditProduct />} />
               <Route path="/DeleteProducts" component={() => <DeleteProduct />} />
--- src/components/login-page/login-page.js
+++ src/components/login-page/login-page.js
@@ -1,27 +1,87 @@
 import './login-page.css';
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import MuiAlert from "@material-ui/lab/Alert";
 
-function LoginPage() {
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+async function loginUser(credentials) {
+
+  return fetch('http://localhost:8080/Users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  }).then(data => data.json())
+ }
+
+export default function LoginPage({ setToken }) {
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState("");
+
+    const handleSubmit = async e => {
+      if (email === "" || password === "") {
+        setError("Fields are required");
+        return;
+      } else {
+        e.preventDefault();
+        const token = await loginUser({
+          email,
+          password
+        });
+        if (token.message !== "Auth successful") {
+          setError("Authentication failed");
+        } else {
+          setToken(token);
+        }
+      }
+    }
+
     return (
-      <div className="center"> 
-        <form className="form-signin">
-          <img className="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width={72} height={72} />
-          <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-          <label htmlFor="inputEmail" className="sr-only">Email address</label>
-          <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
-          <label htmlFor="inputPassword" className="sr-only">Password</label>
-          <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-          <div className="checkbox mb-3">
-            <label>
-              <input type="checkbox" defaultValue="remember-me" /> Remember me
-            </label>
-          </div>
-          <button className="btn btn-lg btn-primary btn-block" type="submit"><Link to="/Home">Sign in</Link></button>
-          <p className="mt-5 mb-3 text-muted">© 2017-2018</p>
-        </form>
-      </div>
+      <div>
+        <img className="background" src="https://www.desktopbackground.org/download/o/2010/12/13/125480_page-2-full-hd-1080p-sport-wallpapers-desktop-backgrounds-hd_1920x1080_h.jpg" alt=""/>
+        <div className="login-container">
+        {/* blue-background-login */}
+       
+        <div className="center "> 
+        <h1>Welcome to Sport Star Admin Site</h1>
+          <form className="form-signin" onSubmit={handleSubmit}>
+            <h1 className="h3 mb-3 font-weight-normal">Please sign in to enter</h1>
+            <br/>
+            <label htmlFor="inputEmail" className="sr-only">Email address</label>
+            <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required 
+                   onChange={e => setEmail(e.target.value)}/>
+            <br/>
+            <label htmlFor="inputPassword" className="sr-only">Password</label>
+            <input type="password" id="inputPassword" className="form-control" placeholder="Password" required 
+                   onChange={e => setPassword(e.target.value)}/>
+            <br/>
+            <div className="checkbox mb-3">
+              <label>
+                <input type="checkbox" defaultValue="remember-me" /> Remember me
+              </label>
+            </div>
+            <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            <p className="mt-5 mb-3 text-muted">© Sport Star </p>
 
+            {error && (
+              <Alert severity="error" onClick={() => setError(null)}>
+                {error}
+              </Alert>
+            )}
+
+          </form>
+        </div>
+        </div>
+      </div>
     )
 }
 
-export default LoginPage;
+LoginPage.propTypes = {
+  setToken: PropTypes.func.isRequired
+}
