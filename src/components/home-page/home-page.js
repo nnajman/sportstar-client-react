a40cf2cf04f3a11ae09d43399b610a23173d220f
Nir Naiman <206769390@STUD.colman.ac.il>
2021-03-12 11:27:48 +0200

    socketIO

--- src/components/home-page/home-page.js
+++ src/components/home-page/home-page.js
@@ -1,20 +1,31 @@
 import './home-page.css';
 import { Link } from "react-router-dom";
+// import socketio from "socket.io-client";
+import { useEffect, useState } from 'react';
 
 function Homepage() {
-    return (
-     <div>
-        <img className="background" src="https://upl.stack.com/wp-content/uploads/2014/03/24130205/stack-getfaster.jpg" alt=""/>
-        <div className="shop-container">
-          <h1 className="title">Hello Manager</h1>
-          <div className="shop-buttons">
-            <button style={{marginRight: '3%'}} className="shop-button"><Link to="/Products">Products</Link></button>
-            <button style={{marginRight: '3%'}} className="shop-button"><Link to="/Categories">Categories</Link></button>
-            <button className="shop-button"><Link to="/Users">Users</Link></button>
-          </div>
+
+//   const [onlineUsers, setOnlineUsers] = useState([]);
+//   const socket = socketio.connect('http://localhost:4200');
+
+//   useEffect(() => {
+//     socket.on("count", data =>
+//     setOnlineUsers(data));
+// }, []);
+
+  return (
+    <div>
+      <img className="background" src="https://upl.stack.com/wp-content/uploads/2014/03/24130205/stack-getfaster.jpg" alt=""/>
+      <div className="shop-container">
+        <h1 className="title">Hello Manager</h1>
+        <div className="shop-buttons">
+          <button style={{marginRight: '3%'}} className="shop-button"><Link to="/Products">Products</Link></button>
+          <button style={{marginRight: '3%'}} className="shop-button"><Link to="/Categories">Categories</Link></button>
+          <button className="shop-button"><Link to="/Users">Users</Link></button>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Homepage;
